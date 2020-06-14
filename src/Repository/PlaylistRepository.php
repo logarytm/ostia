e0cca0355ae37314b98febb3a7353b686f039ddb
Sobak <msobaczewski@gmail.com>
2020-06-14 06:40:19 +0200

    Drop default repository example code

--- src/Repository/PlaylistRepository.php
+++ src/Repository/PlaylistRepository.php
@@ -20,33 +20,4 @@ class PlaylistRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Playlist::class);
     }
-
-    // /**
-    //  * @return Playlist[] Returns an array of Playlist objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Playlist
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
